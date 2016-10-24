333dac07593ce4323c20c49460aca78f3086a48a
Nick Bryan <nickbryan.dev@gmail.com>
2016-10-24 19:48:11 +0100

    Updating routes for blog pages

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@
  * @var Slim\App $app
  */
 use App\Actions\AboutAction;
+use App\Actions\BlogPageAction;
 use App\Actions\HomeAction;
 use App\Actions\TreehouseSaveAction;
 
@@ -13,3 +14,9 @@ $app->get('/', HomeAction::class)->setName('homePage');
 $app->get('/about', AboutAction::class)->setName('aboutPage');
 
 $app->post('/treehouse', TreehouseSaveAction::class);
+
+$app->get('/blog', function(){
+    return 'Blog Index Page';
+});
+
+$app->get('/blog/{slug}', BlogPageAction::class);
